90b339dfed1b6198fb9fdf5098bfa809abb63df4
Tjorben Vandendriessche <Tjorben.vandendriessche@student.howest.be>
2026-08-07 08:37:55 +0200

    b) Add LeagueId FK and League navigation property to Game

--- ExamenOnlineGokken.Domain/Entities/Game.cs
+++ ExamenOnlineGokken.Domain/Entities/Game.cs
@@ -12,5 +12,8 @@ namespace ExamenOnlineGokken.Domain.Entities
         public string AwayTeam { get; set; }
         public DateTime DateOfGame { get; set; }
         public ICollection<Bet> Bets { get; set; }
+
+        public long LeagueId { get; set; }
+        public League League { get; set; }
     }
 }
