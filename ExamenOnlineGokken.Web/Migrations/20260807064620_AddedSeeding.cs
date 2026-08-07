a744bb201931b0f6013fa5dbbd19d7db79a26882
Tjorben Vandendriessche <Tjorben.vandendriessche@student.howest.be>
2026-08-07 08:46:38 +0200

    f) Add AddedSeeding migration

---
+++ ExamenOnlineGokken.Web/Migrations/20260807064620_AddedSeeding.cs
@@ -0,0 +1,275 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ExamenOnlineGokken.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedSeeding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "LeagueId",
+                value: 1L);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "LeagueId",
+                value: 1L);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "LeagueId",
+                value: 1L);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 4L,
+                column: "LeagueId",
+                value: 1L);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 5L,
+                column: "LeagueId",
+                value: 2L);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 6L,
+                column: "LeagueId",
+                value: 2L);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 7L,
+                column: "LeagueId",
+                value: 2L);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 8L,
+                column: "LeagueId",
+                value: 2L);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 9L,
+                column: "LeagueId",
+                value: 3L);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 10L,
+                column: "LeagueId",
+                value: 3L);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 11L,
+                column: "LeagueId",
+                value: 3L);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 12L,
+                column: "LeagueId",
+                value: 3L);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 13L,
+                column: "LeagueId",
+                value: 4L);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 14L,
+                column: "LeagueId",
+                value: 4L);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 15L,
+                column: "LeagueId",
+                value: 4L);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 16L,
+                column: "LeagueId",
+                value: 4L);
+
+            migrationBuilder.InsertData(
+                table: "Leagues",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1L, "Serie A" },
+                    { 2L, "Jupiler League" },
+                    { 3L, "UEFA Champions League" },
+                    { 4L, "UEFA Europa League" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Leagues",
+                keyColumn: "Id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "Leagues",
+                keyColumn: "Id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "Leagues",
+                keyColumn: "Id",
+                keyValue: 3L);
+
+            migrationBuilder.DeleteData(
+                table: "Leagues",
+                keyColumn: "Id",
+                keyValue: 4L);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "LeagueId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "LeagueId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "LeagueId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 4L,
+                column: "LeagueId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 5L,
+                column: "LeagueId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 6L,
+                column: "LeagueId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 7L,
+                column: "LeagueId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 8L,
+                column: "LeagueId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 9L,
+                column: "LeagueId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 10L,
+                column: "LeagueId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 11L,
+                column: "LeagueId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 12L,
+                column: "LeagueId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 13L,
+                column: "LeagueId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 14L,
+                column: "LeagueId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 15L,
+                column: "LeagueId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 16L,
+                column: "LeagueId",
+                value: null);
+        }
+    }
+}
