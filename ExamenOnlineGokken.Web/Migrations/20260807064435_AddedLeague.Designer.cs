33f4437c84c2781ff52f5f300896fa190471bbdf
Tjorben Vandendriessche <Tjorben.vandendriessche@student.howest.be>
2026-08-07 08:44:50 +0200

    c) Add AddedLeague migration

--- ExamenOnlineGokken.Web/Migrations/20260807064435_AddedLeague.Designer.cs
+++ ExamenOnlineGokken.Web/Migrations/20260807064435_AddedLeague.Designer.cs
@@ -4,6 +4,7 @@ using ExamenOnlineGokken.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ExamenOnlineGokken.Web.Migrations
 {
     [DbContext(typeof(GambleDbContext))]
-    partial class GambleDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260807064435_AddedLeague")]
+    partial class AddedLeague
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ExamenOnlineGokken.Web/Migrations/20260807064435_AddedLeague.cs
@@ -0,0 +1,176 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExamenOnlineGokken.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedLeague : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "LeagueId",
+                table: "Games",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Leagues",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Leagues", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "LeagueId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "LeagueId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "LeagueId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 4L,
+                column: "LeagueId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 5L,
+                column: "LeagueId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 6L,
+                column: "LeagueId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 7L,
+                column: "LeagueId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 8L,
+                column: "LeagueId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 9L,
+                column: "LeagueId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 10L,
+                column: "LeagueId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 11L,
+                column: "LeagueId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 12L,
+                column: "LeagueId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 13L,
+                column: "LeagueId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 14L,
+                column: "LeagueId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 15L,
+                column: "LeagueId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Games",
+                keyColumn: "Id",
+                keyValue: 16L,
+                column: "LeagueId",
+                value: null);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_LeagueId",
+                table: "Games",
+                column: "LeagueId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Games_Leagues_LeagueId",
+                table: "Games",
+                column: "LeagueId",
+                principalTable: "Leagues",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Games_Leagues_LeagueId",
+                table: "Games");
+
+            migrationBuilder.DropTable(
+                name: "Leagues");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Games_LeagueId",
+                table: "Games");
+
+            migrationBuilder.DropColumn(
+                name: "LeagueId",
+                table: "Games");
+        }
+    }
+}
