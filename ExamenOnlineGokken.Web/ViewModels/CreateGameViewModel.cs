3d3f68555ab0125506953df59b03f0d881ff7282
Tjorben Vandendriessche <Tjorben.vandendriessche@student.howest.be>
2026-08-09 14:06:44 +0200

    c) Apply Required and NotInThePast validation to CreateGameViewModel

--- ExamenOnlineGokken.Web/ViewModels/CreateGameViewModel.cs
+++ ExamenOnlineGokken.Web/ViewModels/CreateGameViewModel.cs
@@ -1,3 +1,4 @@
+using ExamenOnlineGokken.Validation;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
@@ -8,16 +9,20 @@ namespace ExamenOnlineGokken.ViewModels
     public class CreateGameViewModel
     {
         [Display(Name = "Home team")]
+        [Required]
         public string HomeTeam { get; set; }
 
         [Display(Name = "Away team")]
+        [Required]
         public string AwayTeam { get; set; }
 
         [Display(Name = "Date of game")]
         [DataType(DataType.Date)]
+        [NotInThePast]
         public DateTime DateOfGame { get; set; }
 
         [Display(Name = "League")]
+        [Required]
         public long? SelectedLeagueId { get; set; }
         public List<SelectListItem> Leagues { get; set; }
     }
