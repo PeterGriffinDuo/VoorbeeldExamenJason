99410676c96b3acb9a014d13dbcd88bfa1dc0395
Tjorben Vandendriessche <Tjorben.vandendriessche@student.howest.be>
2026-08-09 15:08:15 +0200

    b) Store the bettingcart as a session list, add ShowBets/Remove and a Bet button

--- ExamenOnlineGokken.Web/Controllers/BetController.cs
+++ ExamenOnlineGokken.Web/Controllers/BetController.cs
@@ -1,19 +1,79 @@
+using ExamenOnlineGokken.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace ExamenOnlineGokken.Controllers
 {
     public class BetController : Controller
     {
-        private const string BET_COUNT_KEY = "BetCount";
+        private const string CART_SESSION_KEY = "BettingCart";
+
+        private readonly GambleDbContext _gambleDbContext;
+
+        public BetController(GambleDbContext gambleDbContext)
+        {
+            _gambleDbContext = gambleDbContext;
+        }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Add(long gameId)
         {
-            int betCount = HttpContext.Session.GetInt32(BET_COUNT_KEY) ?? 0;
-            HttpContext.Session.SetInt32(BET_COUNT_KEY, betCount + 1);
+            var gameIds = GetCartGameIds();
+
+            if (!gameIds.Contains(gameId))
+            {
+                gameIds.Add(gameId);
+                SaveCartGameIds(gameIds);
+            }
 
             return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Remove(long gameId)
+        {
+            var gameIds = GetCartGameIds();
+            gameIds.Remove(gameId);
+            SaveCartGameIds(gameIds);
+
+            return RedirectToAction("ShowBets");
+        }
+
+        public async Task<IActionResult> ShowBets()
+        {
+            var gameIds = GetCartGameIds();
+
+            var games = await _gambleDbContext.Games
+                .Where(g => gameIds.Contains(g.Id))
+                .OrderBy(g => g.DateOfGame)
+                .ToListAsync();
+
+            return View(games);
+        }
+
+        private List<long> GetCartGameIds()
+        {
+            string serializedCart = HttpContext.Session.GetString(CART_SESSION_KEY);
+
+            if (serializedCart == null)
+            {
+                return new List<long>();
+            }
+
+            return JsonConvert.DeserializeObject<List<long>>(serializedCart);
+        }
+
+        private void SaveCartGameIds(List<long> gameIds)
+        {
+            string serializedCart = JsonConvert.SerializeObject(gameIds);
+            HttpContext.Session.SetString(CART_SESSION_KEY, serializedCart);
         }
     }
 }
